a4830e6a3c9ea4a9b392141a9f6ef9c3254761f0
harilal766 <harilalsunil2@gmail.com>
2025-08-11 20:23:15 +0530

    added credentials forms and submit button

--- static/js/form.js
+++ static/js/form.js
@@ -1,15 +1,33 @@
 function storeForms(selection){
     const formDict = {
-        "Amazon" : ("Client Id", "Client Secret", "Refresh Token"),
-        "Shopify" : ("Storename", "Access Token")
+        "Amazon" : ["Client Id", "Client Secret", "Refresh Token"],
+        "Shopify" : ["Storename", "Access Token"]
     }
     try{
         console.log(formDict.key);
-        let credDiv = document.createElement('div');
+        
         let formDiv = document.getElementById('store-form');
+        let credDiv = document.getElementById('credentials');
+
+        credDiv.innerHTML = "";
 
         /* add the input tags to the credential div*/
+        let selectedCreds = formDict[selection];
+        console.log(selectedCreds);
+        selectedCreds.forEach(cred => {
+            let input = document.createElement('input');
+            input.type = "text";
+            input.placeholder = `Enter ${cred}`;
+            input.name = cred; input.className = "form-control";
+            credDiv.appendChild(input);
+            credDiv.appendChild(document.createElement("br"));
+        });
 
+        let submissionDiv = document.getElementById("submission");
+        let submitButton = document.createElement("button");
+        submitButton.innerHTML = `Add store`;
+        submitButton.className = "btn btn-primary";
+        submissionDiv.appendChild(submitButton);
         /* add the credential div and submit button to the form div */
     }
     catch(e) {
