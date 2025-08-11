f489e46f53ffb8dd0e4efee363897f57994e7f07
harilal766 <harilalsunil2@gmail.com>
2025-08-11 20:56:10 +0530

    added storeprofile class, and form input connection to backend

--- amazon/models.py
+++ amazon/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class AmazonCredentials(models.Model):
+    client_id = models.CharField(max_length=200)
+    client_secret = models.CharField(max_length=200)
+    refresh_token = models.CharField(max_length=200)
+    access_token = models.CharField(max_length=200, blank=True, null=True)
+    access_token_refreshing_time = models.DateTimeField(auto_now_add=True)
+    
+    
+    
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from amazon.models import *
+from shopify.models import *
 
 # Create your views here.
 def home(request):
@@ -8,4 +10,18 @@ def add_store(request):
     context = {
         "platforms" : ("Amazon", "Shopify")
     }
+    try:
+        if request.method == "POST":
+            platform = request.POST.get("platform")
+            if platform == "Amazon":
+                pass
+            elif platform == "Shopify":
+                pass
+            
+            storename = request.POST.get("Name of the store")
+            print(storename, platform)
+            
+            redirect('dashboard:home')
+    except Exception as e:
+        print(e)
     return render(request,"add_store.html", context=context)
