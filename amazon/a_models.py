ee452f510cfb36892435a66baf3275fb88ea6fff
harilal766 <harilalsunil2@gmail.com>
2025-08-16 15:39:38 +0530

    migration changes

--- amazon/a_models.py
+++ amazon/a_models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class SpapiCredential(BaseCredential):
-    client_id = models.CharField(max_length=200)
-    client_secret = models.CharField(max_length=200)
-    refresh_token = models.CharField(max_length=200)
-    access_token = models.CharField(max_length=200, blank=True, null=True)
+    client_id = models.CharField(max_length=500)
+    client_secret = models.CharField(max_length=500)
+    refresh_token = models.CharField(max_length=500)
+    access_token = models.CharField(max_length=500, blank=True, null=True)
     access_token_refreshing_time = models.DateTimeField(auto_now_add=True)
