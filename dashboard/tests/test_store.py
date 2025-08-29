276d81924ef9feb200e264c63dfb2e404a333d3c
harilal766 <harilalsunil2@gmail.com>
2025-08-29 17:11:35 +0530

    ss

--- dashboard/tests/test_store.py
+++ dashboard/tests/test_store.py
@@ -4,10 +4,12 @@ from authorization.tests.test_user import TestUser, test_data_json
 # Create your tests here.
 
 class TestStoreProfile(TestUser):
-    def setUp(self):
+    def setUpStore(self):
+        store_creds = {
+            "user" : self.test_user,
+            **test_data_json["store"]
+        }
         self.test_store = StoreProfile.objects.create(
-            user = self.test_user,
-            platform = test_data_json["store"]["platform"],
-            storename = test_data_json["store"]["storename"],
+            **store_creds
         )
-        
+        self.assertTrue(self.test_store.exists())
