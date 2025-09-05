c3329023612bec753c7430ef2ada503570640311
harilal766 <harilalsunil2@gmail.com>
2025-09-05 09:52:08 +0530

    spapi client for report not working

--- amazon/tests/test_views.py
+++ amazon/tests/test_views.py
@@ -3,13 +3,13 @@ from amazon.views import SpapiBase, SpapiOrderClient, SpapiReportClient, permitt
 from pprint import pprint
 from utils import iso_8601_timestamp, iso_8601_converter
 
+from sp_api.base.reportTypes import ReportType
+
 
 class Test_SpapiBase(TestSpapiCredential):
     def setUp(self):
         super(Test_SpapiBase,self).setUp()
         self.test_credentials = self.spapi_inst.get_credentials()
-        self.starting_date = iso_8601_timestamp(4),
-        self.ending_date = iso_8601_timestamp(0)
         
     def test_credentials(self):
         self.assertEqual(type(self.test_credentials), dict)
@@ -22,11 +22,11 @@ class Test_SpapiOrderClient(Test_SpapiBase):
 class Test_SpapiReportClient(Test_SpapiBase):
     def setUp(self):
         super(Test_SpapiReportClient,self).setUp()
-        self.test_api_model = SpapiReportClient(credentials=self.test_credentials)
+        self.test_api_model = SpapiReportClient(credentials=self.test_credentials).api_model
     
     def test_create_report_id(self):
-        id = self.test_api_model.create_report_id(
-            report_type=permitted_amazon_report_types["Order Report"],
-            starting_date=self.starting_date
+        id = self.test_api_model.create_report(
+            reportType = ReportType.GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL,
+            dataStartTime= iso_8601_timestamp(5)
         )
         print(id)
