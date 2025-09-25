4d613e954573976f014a331b7464c6ce572ca636
harilal766 <harilalsunil2@gmail.com>
2025-09-25 17:05:49 +0530

    tried with a dummy tally table

--- dashboard/d_views.py
+++ dashboard/d_views.py
@@ -213,13 +213,12 @@ class StoreReport(View):
                                 if pivot_index and other_pivot_columns:
                                     pivot_df = report_df.pivot_table(
                                         values= other_pivot_columns,
-                                        index=pivot_index,
+index=pivot_index,
                                         aggfunc='sum',margins= True, margins_name='Grand Total'
                                     )
                                     pivot_df = pivot_df.reset_index().rename(columns={pivot_index: 'Row Labels'})
-                                    print(f"pivot : {pivot_df}")
                             elif sheet == 'tally_table':
-                                tally_df = None
+                                tally_df = pivot_df
                     # updation of selected columns 
                     report_profile = ReportProfile.objects.filter(
                         user = request.user, store = selected_store, 
