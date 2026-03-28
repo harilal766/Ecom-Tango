4bb0a752fd425aa716a69a4dfab87fa56cb6230f
karmark <karmark@debian.none>
2026-03-28 16:57:28 +0530

    script working

--- script.sh
+++ script.sh
@@ -7,11 +7,12 @@ VENV="venv"
 VENV_PYTHON="$PROJECT_DIR/$VENV/bin/python3"
 
 # Commands
-cd $PROJECT_DIR
+cd "$PROJECT_DIR"
 
-if [$VENV];
-    python3 -m venv $VENV
-    $VENV_PYTHON -m pip install -r requirements.txt
+if [ ! -d "$VENV" ]; then
+    echo "Creating virtual environment"
+    python3 -m venv "$VENV"
+    "$VENV_PYTHON" -m pip install -r requirements.txt
 fi
 
-$VENV_PYTHON "manage.py runserver 0.0.0.0:8000"
+"$VENV_PYTHON" manage.py runserver 0.0.0.0:8000
